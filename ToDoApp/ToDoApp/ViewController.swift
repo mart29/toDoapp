a480b36def59e0d0f67c0b8e7e6f1c360609e4fb
Martin Foley <foleym10@msu.edu>
2021-04-29 12:51:35 -0400

    Initial commit gitignore

---
+++ ToDoApp/ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Martin Foley on 4/29/21.
+//  Copyright © 2021 Martin Foley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
